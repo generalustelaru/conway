2e723a6f4e3d2efa35491b0c54a52f27eeaa954b
GeneraluStelaru <adrianpope@proton.me>
2023-11-11 10:14:34 +0200

    added play/pause functionality

--- main.js
+++ main.js
@@ -1,53 +1,65 @@
 
-const state = { cells: [] }
+const state = { cells: [], isRunning: false, intervalFunction: null }
+let newGeneration; // extracted for comparison
+const root = document.documentElement;
+const cellGrid = document.querySelector('.cellGrid');
 
 function main() {
     const { cellSize, columns, rows } = config;
-    const root = document.documentElement;
-    // set CSS variables
+    setParameters(cellSize, columns, rows);
+    populateGrid(columns, rows);
+}
+
+function setParameters(cellSize, columns, rows) {
     root.style.setProperty(cellSize.var, cellSize.value + 'px');
     root.style.setProperty(columns.var, columns.value);
     root.style.setProperty(rows.var, rows.value);
+}
 
+function populateGrid(columns, rows) {
     const { seeds } = config;
-    const cellGrid = document.querySelector('.cellGrid')
-    // create cells for state and DOM
     for (let row = 0; row < columns.value; row++) {
 
         for (let col = 0; col < rows.value; col++) {
             const div = document.createElement('div')
-            div.classList.add('cell')
-            div.id = toID(row, col)
+            div.classList.add('cell');
+            div.id = toID(row, col);
             const isSeed = seeds.find(seed => {
                 return seed.row === row && seed.col === col
             })
-            div.classList.add(isSeed ? 'cell__alive' : 'cell__dead')
+            div.classList.add(isSeed ? 'cell__alive' : 'cell__dead');
 
-            cellGrid.appendChild(div)
-            state.cells.push({ row, col, isAlive: isSeed})
+            cellGrid.appendChild(div);
+            state.cells.push({ row, col, isAlive: isSeed});
         }
     }
+}
 
+function runAutomata() {
     const { interval } = config;
-    // run automata
-    const intervalFunction = setInterval(() => {
-        const newGeneration = state.cells.map(cell => {
+    state.intervalFunction = setInterval(() => {
+        // switch to forEach to include side effects
+        newGeneration = state.cells.map(cell => {
             const liveNeighbors = countLiveNeighbors(state.cells, cell);
             return {
                 ...cell,
                 isAlive: cell.isAlive
                     ? [2, 3].includes(liveNeighbors) ? true : false
                     : liveNeighbors === 3 ? true : false
-            };
+            }
         });
         state.cells = newGeneration;
         updateClasses(newGeneration);
     }, interval);
+}
 
-    const { iterations } = config;
-    // stop automata after iterations complete
-    setTimeout(() => {
-        clearInterval(intervalFunction);
-        console.info('Simulation complete after', iterations, 'iterations');
-    }, (iterations) * interval);
+function toggleRun() {
+    if (state.intervalFunction) {
+        clearInterval(state.intervalFunction);
+        state.intervalFunction = null;
+        console.log('stopped');
+    } else {
+        runAutomata();
+        console.log('running');
+    }
 }
